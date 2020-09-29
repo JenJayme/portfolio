67bb6f077b26923bb459e80e8396051559f731de
Jen Jayme <jeniferjayme@gmail.com>
2020-09-28 19:47:28 -0700

    Adjusted contact modal

--- script.js
+++ script.js
@@ -6,16 +6,11 @@
 //on click, show nav bar and divs with project cards
 //on hover over project cards, darken and slight animate
 var portfolioLink = document.getElementById("portfolio");
-// var video = document.getElementById("myVideo");
-// var pauseButton = document.getElementById("pauseBtn");
-var video = $('#myVideo');
-var pauseButton = $('#pauseBtn');
-var aboutBtn = $('#aboutBtn').get(0);
-var biocard = $('#biocard').get(0);
-// function hideVideo() {
-//     video.setAttribute("hide");
-//     portfolioLink.setAttribute("show");
-// }
+var video = document.getElementById("myVideo");
+var pauseButton = document.getElementById("pauseBtn");
+var aboutBtn = document.getElementById("aboutBtn");
+var biocard = document.getElementById("biocard");
+
 
 function pauseVid() {
     "use strict";
@@ -68,17 +63,17 @@ function setUp() {
     });
 }
 
-// function bioPop() {
-//     "use strict";
-//     $('#aboutBtn').click(function () {
-//         $('popOutRow').removeClass('hidden');
-//         $('popOutRow').addClass('visible');
-//         console.log('Running bioPop function.');
-//         $('#biocard').removeClass('scale-out');
-//         $('#biocard').addClass('scale-in');
-//         console.log('Scale-in class added to biocard.');
-//     });
-// }
+function bioPop() {
+    "use strict";
+    $('#aboutBtn').click(function () {
+        $('popOutRow').removeClass('hidden');
+        $('popOutRow').addClass('visible');
+        console.log('Running bioPop function.');
+        $('#biocard').removeClass('scale-out');
+        $('#biocard').addClass('scale-in');
+        console.log('Scale-in class added to biocard.');
+    });
+}
 
 
 $(document).ready(function () {
